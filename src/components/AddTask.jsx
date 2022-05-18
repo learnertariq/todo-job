93ea317f9117665471dd74bbed27fbc264a05d5c
Tariqul Islam <usukuk@gmail.com>
2022-05-18 23:59:05 +0600

    .env to secure data

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -3,16 +3,16 @@ import { Button, Form } from "react-bootstrap";
 
 const AddTask = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form className="mb-5 mt-3">
+      <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Control type="text" placeholder="Add Task" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Add Task" />
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Control as="textarea" rows={3} placeholder="Task Description" />
       </Form.Group>
 
       <Button variant="primary" type="submit">
-        Submit
+        Add Task
       </Button>
     </Form>
   );
